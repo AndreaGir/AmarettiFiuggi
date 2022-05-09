fbe2bea8c1011d99e2d6f45615afa7d7136cc232
MatteoRossi01 <93929073+MatteoRossi01@users.noreply.github.com>
2022-05-10 00:52:50 +0200

    Aggiunta vue-router

---
+++ resources/js/pages/Products.vue
@@ -0,0 +1,28 @@
+<template>
+    <section class="products_section">
+    <Header/>
+    <main>
+      Prodotti
+    </main>
+    <Footer/>
+  </section>
+</template>
+
+<script>
+import Header from '../components/Header.vue';
+import Footer from '../components/Footer.vue';
+import Main from '../components/Main.vue';
+
+export default {
+    name: 'Products',
+
+    components: {
+      Header,
+      Footer,
+        Main
+    } 
+}
+</script>
+
+<style>
+</style>
